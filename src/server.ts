65b6101408e90b47d1afd2f5674099d0534e8854
Gabi Poncet <gabiponcet@gmail.com>
2021-02-22 11:08:22 -0300

    first NLW4 day: installations, routes, insomnia, ts

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express, { request, response } from 'express';
+
+const app = express();
+
+app.get('/', (request, response) => {
+
+    return response.json( {message: "Hello, world!" });
+});
+
+app.post('/', (request, response) => {
+    return response.json({ message: "Dados atualizados."});
+});
+
+app.listen(3333, () => console.log('Server ON!!!!'));
